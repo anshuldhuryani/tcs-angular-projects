3f0d397c00e165510a39ece72002c26069fa785a
bgunwani <bhawna.gunwani@gmail.com>
2021-06-28 22:24:08 +0530

    Day One Angular

---
+++ angular-dayone-project/src/app/first.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+// Component Decorator
+@Component({
+    selector: 'app-first',
+    template: '<h2>First Component</h2>',
+    styles: ['h2 {color:navy; }']
+})
+
+export class FirstComponent {
+
+    // Logics Here..
+
+}
